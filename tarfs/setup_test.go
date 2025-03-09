08725d8d720b74ec2aac440ef37c226c038b529c
ngicks <yknt.bsl@gmail.com>
2025-03-09 12:36:53 +0900

    ✅(tarfs): stop copying testdata tars
    It was unnecessary.
    I think I was going to embed tars with go:embed so I thought I needed to copy them.

--- tarfs/setup_test.go
+++ tarfs/setup_test.go
@@ -5,7 +5,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io/fs"
+	"io"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -16,21 +16,88 @@ import (
 var (
 	curMod   GoMod
 	testTars = sync.OnceValues(func() ([]string, error) {
-		dir := filepath.Join("testdata", "go"+curMod.Go)
-		dirents, err := os.ReadDir(dir)
+		testdataDir, err := stdTarTestDir(curMod.Go)
+		if err != nil {
+			return nil, err
+		}
+		dirents, err := os.ReadDir(testdataDir)
 		if err != nil {
 			return nil, err
 		}
 		names := make([]string, 0, len(dirents))
 		for _, dirent := range dirents {
-			if dirent.IsDir() || !strings.HasSuffix(dirent.Name(), ".tar") {
+			if !dirent.Type().IsRegular() || !strings.HasSuffix(dirent.Name(), ".tar") {
 				continue
 			}
-			names = append(names, filepath.Join(dir, dirent.Name()))
+			names = append(names, filepath.Join(testdataDir, dirent.Name()))
 		}
 		return names, nil
 	})
 )
+
+func stdTarTestDir(version string) (string, error) {
+	goroot, err := exec.Command("go"+version, "env", "GOROOT").Output()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(filepath.FromSlash(strings.TrimSpace(string(goroot))), "src", "archive", "tar", "testdata"), nil
+}
+
+// reads go version recodred in go.mod, install sdk of that version.
+func init() {
+	cmd := exec.CommandContext(context.Background(), "go", "mod", "edit", "-json")
+	p, err := cmd.StdoutPipe()
+	if err != nil {
+		panic(err)
+	}
+	defer p.Close()
+	if err := cmd.Start(); err != nil {
+		panic(err)
+	}
+	dec := json.NewDecoder(p)
+	if err = dec.Decode(&curMod); err != nil {
+		if cmd.Cancel != nil {
+			_ = cmd.Cancel()
+		} else {
+			_ = cmd.Process.Kill()
+		}
+		_, _ = io.Copy(io.Discard, p)
+		_ = cmd.Wait()
+		panic(err)
+	}
+	if err := cmd.Wait(); err != nil {
+		panic(err)
+	}
+
+	resourceDir, err := stdTarTestDir(curMod.Go)
+	if err == nil {
+		_, err := os.Stat(resourceDir)
+		if err == nil {
+			dirents, err := os.ReadDir(resourceDir)
+			if err != nil {
+				panic(err)
+			}
+			if len(dirents) > 0 {
+				return
+			}
+		}
+	}
+
+	if _, err := exec.Command("go", "install", fmt.Sprintf("golang.org/dl/go%s@latest", curMod.Go)).Output(); err != nil {
+		printPanic(err)
+	}
+	if _, err := exec.Command("go"+curMod.Go, "download").Output(); err != nil {
+		printPanic(err)
+	}
+}
+
+func printPanic(err error) {
+	var exitErr *exec.ExitError
+	if errors.As(err, &exitErr) {
+		fmt.Printf("stderr = %s\n", exitErr.Stderr)
+	}
+	panic(err)
+}
 
 type GoMod struct {
 	Module    ModPath
@@ -77,70 +144,4 @@ type Retract struct {
 
 type Tool struct {
 	Path string
-}
-
-// reads go version recodred in go.mod, install sdk of that version.
-// Assuming sdk is already under $PATH, it invokes go${version} download, copies contentes under $(go{version} env GOROOT)/src/archive/tar/testdata/* to
-// ./testdata/go${version}
-func init() {
-	cmd := exec.CommandContext(context.Background(), "go", "mod", "edit", "-json")
-	p, err := cmd.StdoutPipe()
-	if err != nil {
-		panic(err)
-	}
-	defer p.Close()
-	if err := cmd.Start(); err != nil {
-		panic(err)
-	}
-	dec := json.NewDecoder(p)
-	if err = dec.Decode(&curMod); err != nil {
-		if cmd.Cancel != nil {
-			_ = cmd.Cancel()
-		} else {
-			_ = cmd.Process.Kill()
-		}
-		panic(err)
-	}
-	if err := cmd.Wait(); err != nil {
-		panic(err)
-	}
-
-	resourceDir := filepath.Join("testdata", "go"+curMod.Go)
-
-	if _, err := os.Stat(resourceDir); err == nil {
-		dirents, err := os.ReadDir(resourceDir)
-		if err != nil {
-			panic(err)
-		}
-		if len(dirents) > 0 {
-			return
-		}
-	}
-
-	if err := os.MkdirAll(resourceDir, fs.ModePerm); err != nil {
-		panic(err)
-	}
-
-	if _, err := exec.Command("go", "install", fmt.Sprintf("golang.org/dl/go%s@latest", curMod.Go)).Output(); err != nil {
-		printPanic(err)
-	}
-	if _, err := exec.Command("go"+curMod.Go, "download").Output(); err != nil {
-		printPanic(err)
-	}
-	dir, err := exec.Command("go"+curMod.Go, "env", "GOROOT").Output()
-	if err != nil {
-		printPanic(err)
-	}
-	err = os.CopyFS(resourceDir, os.DirFS(filepath.Join(filepath.FromSlash(strings.TrimSpace(string(dir))), "src", "archive", "tar", "testdata")))
-	if err != nil {
-		printPanic(err)
-	}
-}
-
-func printPanic(err error) {
-	var exitErr *exec.ExitError
-	if errors.As(err, &exitErr) {
-		fmt.Printf("stderr = %s\n", exitErr.Stderr)
-	}
-	panic(err)
 }
