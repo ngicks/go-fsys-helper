c07d80a20332793b0507495ec7c7fa1cdbfb577e
ngicks <yknt.bsl@gmail.com>
2025-05-28 09:30:02 +0900

    ✨(vroot): add vroot to fs.FS converter

---
+++ vroot/iofs.go
@@ -0,0 +1,124 @@
+package vroot
+
+import (
+	"io"
+	"io/fs"
+	"path/filepath"
+)
+
+var (
+	_ fs.FS         = (*IoFsRooted)(nil)
+	_ fs.ReadDirFS  = (*IoFsRooted)(nil)
+	_ fs.ReadFileFS = (*IoFsRooted)(nil)
+	_ fs.ReadLinkFS = (*IoFsRooted)(nil)
+	_ fs.StatFS     = (*IoFsRooted)(nil)
+	_ fs.SubFS      = (*IoFsRooted)(nil)
+)
+
+type IoFsRooted struct {
+	root Rooted
+}
+
+func NewIoFsRooted(root Rooted) *IoFsRooted {
+	return &IoFsRooted{root: root}
+}
+
+func (fsys *IoFsRooted) Close() error {
+	return fsys.root.Close()
+}
+
+func (fsys *IoFsRooted) Open(name string) (fs.File, error) {
+	return newReadOnlyFile(fsys.root.Open(filepath.FromSlash(name)))
+}
+
+func (fsys *IoFsRooted) ReadDir(name string) ([]fs.DirEntry, error) {
+	f, err := fsys.root.Open(filepath.FromSlash(name))
+	if err != nil {
+		return []fs.DirEntry{}, err
+	}
+	defer f.Close()
+	return f.ReadDir(-1)
+}
+
+func (fsys *IoFsRooted) ReadFile(name string) ([]byte, error) {
+	f, err := fsys.root.Open(filepath.FromSlash(name))
+	if err != nil {
+		return []byte{}, err
+	}
+	defer f.Close()
+	return io.ReadAll(f)
+}
+
+func (fsys *IoFsRooted) ReadLink(name string) (string, error) {
+	return fsys.root.Readlink(filepath.FromSlash(name))
+}
+
+func (fsys *IoFsRooted) Lstat(name string) (fs.FileInfo, error) {
+	return fsys.root.Lstat(filepath.FromSlash(name))
+}
+
+func (fsys *IoFsRooted) Stat(name string) (fs.FileInfo, error) {
+	return fsys.root.Stat(filepath.FromSlash(name))
+}
+
+func (fsys *IoFsRooted) Sub(dir string) (fs.FS, error) {
+	root, err := fsys.root.OpenRoot(filepath.FromSlash(dir))
+	if err != nil {
+		return nil, err
+	}
+	return NewIoFsRooted(root), nil
+}
+
+type IoFsUnrooted struct {
+	root Unrooted
+}
+
+func NewIoFsUnrooted(root Unrooted) *IoFsUnrooted {
+	return &IoFsUnrooted{root: root}
+}
+
+func (fsys *IoFsUnrooted) Close() error {
+	return fsys.root.Close()
+}
+
+func (fsys *IoFsUnrooted) Open(name string) (fs.File, error) {
+	return newReadOnlyFile(fsys.root.Open(filepath.FromSlash(name)))
+}
+
+func (fsys *IoFsUnrooted) ReadDir(name string) ([]fs.DirEntry, error) {
+	f, err := fsys.root.Open(filepath.FromSlash(name))
+	if err != nil {
+		return []fs.DirEntry{}, err
+	}
+	defer f.Close()
+	return f.ReadDir(-1)
+}
+
+func (fsys *IoFsUnrooted) ReadFile(name string) ([]byte, error) {
+	f, err := fsys.root.Open(filepath.FromSlash(name))
+	if err != nil {
+		return []byte{}, err
+	}
+	defer f.Close()
+	return io.ReadAll(f)
+}
+
+func (fsys *IoFsUnrooted) ReadLink(name string) (string, error) {
+	return fsys.root.Readlink(filepath.FromSlash(name))
+}
+
+func (fsys *IoFsUnrooted) Lstat(name string) (fs.FileInfo, error) {
+	return fsys.root.Lstat(filepath.FromSlash(name))
+}
+
+func (fsys *IoFsUnrooted) Stat(name string) (fs.FileInfo, error) {
+	return fsys.root.Stat(filepath.FromSlash(name))
+}
+
+func (fsys *IoFsUnrooted) Sub(dir string) (fs.FS, error) {
+	root, err := fsys.root.OpenUnrooted(filepath.FromSlash(dir))
+	if err != nil {
+		return nil, err
+	}
+	return NewIoFsUnrooted(root), nil
+}
---
+++ vroot/iofs_test.go
@@ -0,0 +1,54 @@
+package vroot_test
+
+import (
+	"path/filepath"
+	"strings"
+	"testing"
+	"testing/fstest"
+
+	"github.com/ngicks/go-fsys-helper/vroot"
+	"github.com/ngicks/go-fsys-helper/vroot/acceptancetest"
+	"github.com/ngicks/go-fsys-helper/vroot/internal/prepare"
+	"github.com/ngicks/go-fsys-helper/vroot/osfs"
+)
+
+var readbleFiles []string
+
+func init() {
+	for _, txt := range acceptancetest.RootFsys {
+		if !strings.HasPrefix(txt, "root/readable") {
+			continue
+		}
+		switch {
+		case strings.Contains(txt, ": "):
+			idx := strings.Index(txt, ": ")
+			path := txt[:idx]
+			readbleFiles = append(readbleFiles, strings.TrimPrefix(path, "root/readable/"))
+		}
+	}
+}
+
+func TestIoFsRooted(t *testing.T) {
+	tempDir := t.TempDir()
+	t.Logf("temp dir = %s", tempDir)
+	prepare.MakeFsys(tempDir, true, false)
+	r, err := osfs.NewRooted(filepath.Join(tempDir, "root", "readable"))
+	if err != nil {
+		panic(err)
+	}
+	defer r.Close()
+	fsys := vroot.NewIoFsRooted(r)
+	fstest.TestFS(fsys, readbleFiles...)
+}
+
+func TestIoFsUnrooted(t *testing.T) {
+	tempDir := t.TempDir()
+	t.Logf("temp dir = %s", tempDir)
+	prepare.MakeFsys(tempDir, true, false)
+	r, err := osfs.NewUnrooted(filepath.Join(tempDir, "root", "readable"))
+	if err != nil {
+		panic(err)
+	}
+	fsys := vroot.NewIoFsUnrooted(r)
+	fstest.TestFS(fsys, readbleFiles...)
+}
